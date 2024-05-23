0f12f516b34faf1a17a2890f630c10504f95a54a
tankroo1 <tankroo.llc@gmail.com>
2024-05-22 20:38:10 -0500

    third commit

---
+++ main.tf
@@ -0,0 +1,81 @@
+/*Create a publically accesible bucket in GCP with Terraform.  You must complete the following tasks.
+1) Terraform script
+2) Git Push the script to your Github
+3) Outpub file must show the public link
+4) Must have an index.html file within
+*/
+
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.28.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  region = "us-central1"
+  project =  "viceroygunray"
+  zone = "us-central1-a"
+  credentials = "viceroygunray-712c1dbcf854.json"
+}
+
+resource "google_storage_bucket" "buckybucket" {
+  name          = "crazy_8"
+  location      = "us-central1"
+  force_destroy = true
+
+  uniform_bucket_level_access = false
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
+
+// Setting the bucket ACL to public read
+resource "google_storage_bucket_acl" "bucket_acl" {
+  bucket         = google_storage_bucket.buckybucket.name
+  predefined_acl = "publicRead"
+}
+
+// Uploading and setting public read access for HTML files
+resource "google_storage_bucket_object" "upload_html" {
+  for_each     = fileset("${path.module}/", "*.html")
+  bucket       = google_storage_bucket.buckybucket.name
+  name         = each.value
+  source       = "${path.module}/${each.value}"
+  content_type = "text/html"
+}
+
+// Public ACL for each HTML file
+resource "google_storage_object_acl" "html_acl" {
+  for_each       = google_storage_bucket_object.upload_html
+  bucket         = google_storage_bucket_object.upload_html[each.key].bucket
+  object         = google_storage_bucket_object.upload_html[each.key].name
+  predefined_acl = "publicRead"
+}
+
+// Uploading and setting public read access for image files
+resource "google_storage_bucket_object" "upload_images" {
+  for_each     = fileset("${path.module}/", "*.jpg")
+  bucket       = google_storage_bucket.buckybucket.name
+  name         = each.value
+  source       = "${path.module}/${each.value}"
+  content_type = "image/jpeg"
+}
+
+// Public ACL for each image file
+resource "google_storage_object_acl" "image_acl" {
+  for_each       = google_storage_bucket_object.upload_images
+  bucket         = google_storage_bucket_object.upload_images[each.key].bucket
+  object         = google_storage_bucket_object.upload_images[each.key].name
+  predefined_acl = "publicRead"
+}
+
+output "website_url" {
+  value = "https://storage.googleapis.com/${google_storage_bucket.buckybucket.name}/index.html"
+}
+
